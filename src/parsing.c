9a5e1f86736e3551ec0d9590771a79f0ff83a257
SergioBonatto <bonatto@tutanota.com>
2026-01-06 17:49:19 -0300

    add: evaluation

--- src/parsing.c
+++ src/parsing.c
@@ -25,6 +25,37 @@ void add_history(char* unused){}
 #include <editline/readline.h>
 #endif
 
+
+
+long eval_op(long x, char* op, long y){
+    if (strcmp(op, "+") == 0) {return x + y;}
+    if (strcmp(op, "-") == 0) {return x - y;}
+    if (strcmp(op, "*") == 0) {return x * y;}
+    if (strcmp(op, "/") == 0) {return x / y;}
+    return 0;
+}
+
+long eval(mpc_ast_t* t){
+    
+    // if tagged as number, return it directly
+    if (strstr(t->tag, "number"))
+        return atoi(t->contents);
+
+    // the operator is always second child
+    char* op = t->children[1]->contents;
+
+    // we store the third child in `x`
+    long x = eval(t->children[2]);
+
+    // iterate the remainind children and combining
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")){
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+    return x;
+}
+
 int main(int argc, char** argv){
 
     // create some Parsers
@@ -60,8 +91,11 @@ int main(int argc, char** argv){
         // attempt to parse the user input
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Praxis, &r)){
+
+            long result = eval(r.output);
+            printf("%li\n", result);
             // on success print the AST
-            mpc_ast_print(r.output);
+            /* mpc_ast_print(r.output); */
             mpc_ast_delete(r.output);
         } else {
             // otherwise print the error
