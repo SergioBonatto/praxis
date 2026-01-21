d6f302a8e74f4f2b52e94abb28c3b895ded7aac2
SergioBonatto <bonatto@tutanota.com>
2026-01-21 13:55:06 -0300

    add: cmp and ord

--- src/parsing.c
+++ src/parsing.c
@@ -728,6 +728,8 @@ lval* builtin_join(lenv* e, lval* a){
 */
 
 lval* builtin_op(lenv* e, lval* a, char* op);
+lval* builtin_ord(lenv* e, lval*a, char* ord);
+lval* builtin_cmp(lenv* e, lval* a, char* op);
 
 lval* builtin_add(lenv* e, lval* a){
     return builtin_op(e, a, "+");
@@ -744,6 +746,31 @@ lval* builtin_mul(lenv* e, lval* a){
 lval* builtin_div(lenv* e, lval* a){
     return builtin_op(e, a, "/");
 }
+
+lval* builtin_gt(lenv* e, lval* a){
+    return builtin_ord(e, a, ">");
+}
+
+lval* builtin_lt(lenv* e, lval* a){
+    return builtin_ord(e, a, "<");
+}
+
+lval* builtin_gte(lenv* e, lval* a){
+    return builtin_ord(e, a, ">=");
+}
+
+lval* builtin_lte(lenv* e, lval* a){
+    return builtin_ord(e, a, "<=");
+}
+
+lval* builtin_eq(lenv* e, lval* a){
+    return builtin_cmp(e, a, "==");
+}
+
+lval* builtin_neq(lenv* e, lval* a){
+    return builtin_cmp(e, a, "!=");
+}
+
 
 lval* builtin_op(lenv* e, lval* a, char* op){
     // ensure all arguments are numbers
@@ -780,6 +807,74 @@ lval* builtin_op(lenv* e, lval* a, char* op){
     }
     lval_del(a);
     return x;
+}
+
+lval* builtin_ord(lenv* e, lval*a, char* op){
+    LASSERT_NUM(op, a, 2);
+    LASSERT_TYPE(op, a, 0, LVAL_NUM);
+    LASSERT_TYPE(op, a, 2, LVAL_NUM);
+
+    int r;
+    if(strcmp(op, ">") == 0){
+        r = (a->cell[0]->num > a->cell[1]->num);
+    }
+    if(strcmp(op, "<") == 0){
+        r = (a->cell[0]->num < a->cell[1]->num);
+    }
+   if(strcmp(op, ">=") == 0){
+        r = (a->cell[0]->num > a->cell[1]->num);
+    }
+   if(strcmp(op, "<=") == 0){
+        r = (a->cell[0]->num > a->cell[1]->num);
+    }
+   lval_del(a);
+   return lval_num(r);
+}
+
+int lval_eq(lval* x, lval* y){
+    if (x->type != y->type){ return 0; }
+
+    switch (x->type){
+        case LVAL_NUM: 
+            return (x->num == y->num);
+
+        case LVAL_ERR:
+            return (strcmp(x->err, y->err) == 0);
+
+        case LVAL_SYM:
+            return (strcmp(x->sym, y->sym) == 0);
+
+        case LVAL_FUN:
+            if(x->builtin || y->builtin){
+                return x->builtin == y->builtin;
+            } else {
+                return lval_eq(x->formals, y->formals)
+                    && lval_eq(x->body, y->body);
+            }
+
+        case LVAL_QEXPR:
+        case LVAL_SEXPR:
+            if (x->count != y->count) { return 0; }
+            for (int i = 0; i < x->count; i++){
+                if(!lval_eq(x->cell[i], y->cell[i])) { return 0; }
+            }
+            return 1;
+        break;
+    }
+    return 0;
+}
+
+lval* builtin_cmp(lenv* e, lval* a, char* op){
+    LASSERT_NUM(op, a, 2);
+    int r;
+    if (strcmp(op, "==") == 0){
+        r = lval_eq(a->cell[0], a->cell[1]);
+    }
+    if (strcmp(op, "!=") == 0){
+        r = !lval_eq(a->cell[0], a->cell[1]);
+    }
+    lval_del(a);
+    return lval_num(r);
 }
 
 lval* builtin_lambda(lenv* e, lval* a){
